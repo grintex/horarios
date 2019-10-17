73b3f26463b5b82c9f90da2201e90f4db1f5027b
Fernando Bevilacqua <dovyski@gmail.com>
2019-10-17 15:12:35 -0300

    Switch to gridster for faster prototyping

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,17 @@
-var gridster = null;
-$(document).ready(function () {
-    gridster = $(".gridster ul").gridster({
-        widget_base_dimensions: ['auto', 140],
-        autogenerate_stylesheet: true,
-        min_cols: 1,
-        max_cols: 6,
+var gridster = [];
+
+$(function () {
+    gridster[0] = $("#demo-1 ul").gridster({
+        namespace: '#demo-1',
+        widget_base_dimensions: [400, 100],
         widget_margins: [5, 5],
-        resize: {
-            enabled: true
-        }
+        max_cols: 6,
+        min_rows: 6
     }).data('gridster');
-    $('.gridster  ul').css({'padding': '0'});
+
+    gridster[1] = $("#demo-2 ul").gridster({
+        namespace: '#demo-2',
+        widget_base_dimensions: [200, 110],
+        widget_margins: [10, 10]
+    }).data('gridster');
 });
