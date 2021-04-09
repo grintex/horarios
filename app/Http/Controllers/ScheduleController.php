1460a5a99829ea15608ab5fe887e0ade099da9fe
Fernando Bevilacqua <dovyski@gmail.com>
2021-04-09 07:43:07 -0300

    feat(config) adiciona novo período de horários

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -100,7 +100,7 @@ class ScheduleController extends Controller
             'user_id' => $user->id,
             'name' => '',
             'revision' => 0,
-            'period' => '2020.2',
+            'period' => '2021.1',
             'courses' => '[]',
             'groups' => '[]',
             'relations' => '%.ch',
