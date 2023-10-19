4e7ebc2cad3d3af978abee882a8def03fd4e1f00
Guilherme Dal Bianco <dbguilherme@gmail.com>
2023-10-19 16:25:59 -0300

    Update ScheduleController.php

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -100,7 +100,7 @@ class ScheduleController extends Controller
             'user_id' => $user->id,
             'name' => '',
             'revision' => 0,
-            'period' => '2022.2',
+            'period' => '2024.1',
             'courses' => '[]',
             'groups' => '[]',
             'relations' => '%.ch',
